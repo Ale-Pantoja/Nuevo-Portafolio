361bbabb1b05bf4cbdc698378dc2fcad7f54dfbc
Ale Pantoja <pantojaalejandra1301@gmail.com>
2026-01-12 16:56:01 -0400

    prepare to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Ale-Pantoja.github.io',
-  base: '/Nuevo-Portafolio/',
+  base: '/Nuevo-Portafolio',
   vite: {
     plugins: [tailwindcss()]
   },
